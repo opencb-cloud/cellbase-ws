12d735f3f5f154b25b51cd0185ea71c10a657ad5
Alex <ademaria@cipf.es>
2011-08-02 15:07:07 +0200

    Developing basic methods in CORE branch

---
+++ src/main/java/org/bioinfo/infrared/ws/server/rest/feature/TranscriptWSServer.java
@@ -0,0 +1,90 @@
+package org.bioinfo.infrared.ws.server.rest.feature;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.bioinfo.commons.utils.StringUtils;
+import org.bioinfo.infrared.core.ExonDBAdapter;
+import org.bioinfo.infrared.core.GeneDBAdapter;
+import org.bioinfo.infrared.core.Orthologous;
+import org.bioinfo.infrared.core.TranscriptDBAdapter;
+import org.bioinfo.infrared.core.Exon2TranscriptDBAdapter;
+
+import org.bioinfo.infrared.ws.server.rest.GenericRestWSServer;
+import org.bioinfo.infrared.ws.server.rest.exception.VersionException;
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+
+import com.sun.jersey.api.client.ClientResponse.Status;
+
+@Path("/{version}/{species}/feature/transcript")
+@Produces("text/plain")
+public class TranscriptWSServer extends GenericRestWSServer {
+	
+	
+	public TranscriptWSServer(@PathParam("version") String version, @PathParam("species") String species, @Context UriInfo uriInfo) throws VersionException, IOException {
+		super(version, species, uriInfo);
+	}
+	
+	@GET
+	@Path("/{transcriptId}/info")
+	public Response getByEnsemblId(@PathParam("transcriptId") String query) {
+		try {
+			System.out.println("transcriptId " + "info");
+			return  generateResponse(query, new TranscriptDBAdapter().getByIdList(StringUtils.toList(query, ",")));
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	@GET
+	@Path("/{transcriptId}/gene")
+	public Response getByGene(@PathParam("transcriptId") String query) {
+		try {
+			return  generateResponse(query, new GeneDBAdapter().getGeneByTranscriptList(StringUtils.toList(query, ",")));
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+
+
+	@GET
+	@Path("/{transcriptId}/exon")
+	public Response getExonsByEnsemblId2(@PathParam("transcriptId") String query) {
+		try {
+			return  generateResponse(query, new ExonDBAdapter().getByTranscriptIdList(StringUtils.toList(query, ",")));
+			
+			/** HQL 
+			Query query = this.getSession().createQuery("select e from Exon e JOIN FETCH e.exon2transcripts et JOIN et.transcript t JOIN  t.gene g where g.stableId in :stable_id").setParameterList("stable_id", StringUtils.toList(geneId, ","));  
+			return generateResponse(query);
+			**/
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@GET
+	@Path("/{geneId}/exon2transcript")
+	public Response getExon2TranscriptByEnsemblId(@PathParam("geneId") String query) {
+		try {
+			return  generateResponse(query, new Exon2TranscriptDBAdapter().getByTranscriptIdList(StringUtils.toList(query, ",")));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+
+
+}
