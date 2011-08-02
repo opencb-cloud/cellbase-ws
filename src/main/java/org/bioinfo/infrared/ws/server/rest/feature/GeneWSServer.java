a78804c1ee643e05e1a7a4311e6a6234324a3ce4
Alex <ademaria@cipf.es>
2011-08-02 15:07:00 +0200

    Developing basic methods in CORE branch

--- src/main/java/org/bioinfo/infrared/ws/server/rest/feature/GeneWSServer.java
+++ src/main/java/org/bioinfo/infrared/ws/server/rest/feature/GeneWSServer.java
@@ -1,7 +1,6 @@
 package org.bioinfo.infrared.ws.server.rest.feature;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,27 +10,16 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.bioinfo.commons.utils.StringUtils;
-import org.bioinfo.infrared.core.Exon;
-import org.bioinfo.infrared.core.Exon2transcript;
-import org.bioinfo.infrared.core.Gene;
-import org.bioinfo.infrared.core.GeneDataAdapter;
-import org.bioinfo.infrared.core.TranscriptDataAdapter;
-import org.bioinfo.infrared.core.Orthologous;
-import org.bioinfo.infrared.core.Transcript;
-import org.bioinfo.infrared.dao.GenomeSequenceDataAdapter;
+import org.bioinfo.infrared.core.ExonDBAdapter;
+import org.bioinfo.infrared.core.GeneDBAdapter;
+import org.bioinfo.infrared.core.OrthologousDBAdapter;
+import org.bioinfo.infrared.core.Exon2TranscriptDBAdapter;
+import org.bioinfo.infrared.core.TranscriptDBAdapter;
+import org.bioinfo.infrared.common.dao.GenomeSequenceDataAdapter;
 import org.bioinfo.infrared.ws.server.rest.GenericRestWSServer;
 import org.bioinfo.infrared.ws.server.rest.exception.VersionException;
-import org.hibernate.Criteria;
-import org.hibernate.FetchMode;
-import org.hibernate.Query;
-import org.hibernate.criterion.Disjunction;
-import org.hibernate.criterion.Restrictions;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.sun.jersey.api.client.ClientResponse.Status;
 
 @Path("/{version}/{species}/feature/gene")
@@ -43,25 +31,21 @@ public class GeneWSServer extends GenericRestWSServer {
 		super(version, species, uriInfo);
 	}
 	
-	
 	@GET
 	@Path("/{geneId}/info")
-	public Response getByEnsemblId(@PathParam("geneId") String geneId) {
+	public Response getByEnsemblId(@PathParam("geneId") String query) {
 		try {
-			return  generateResponse(geneId, new GeneDataAdapter().getGeneByIds(geneId));//generateResponse(criteria);
-
+			return  generateResponse(query, new GeneDBAdapter().getGeneByIdList(StringUtils.toList(query, ",")));
 		} catch (Exception e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-	
-	
 
 	@GET
 	@Path("/{geneId}/transcript")
 	public Response getTranscriptsByEnsemblId(@PathParam("geneId") String query) {
 		try {
-			return  generateResponse(query, new TranscriptDataAdapter().getByGenes(query));
+			return  generateResponse(query, new TranscriptDBAdapter().getByGeneIdList(StringUtils.toList(query, ",")));
 		} catch (Exception e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
@@ -69,15 +53,9 @@ public class GeneWSServer extends GenericRestWSServer {
 
 	@GET
 	@Path("/{geneId}/exon")
-	public Response getExonsByEnsemblId2(@PathParam("geneId") String geneId) {
+	public Response getExonsByEnsemblId2(@PathParam("geneId") String query) {
 		try {
-			
-			Criteria criteria =  this.getSession().createCriteria(Exon.class).setFetchMode("exon2transcripts", FetchMode.SELECT)
-			.createCriteria("exon2transcripts").setFetchMode("transcript", FetchMode.SELECT)
-			.createCriteria("transcript").setFetchMode("gene", FetchMode.SELECT)
-			.createCriteria("gene").add( Restrictions.eq("stableId", geneId));
-			return generateResponse(criteria);
-			
+			return generateResponse(query, new ExonDBAdapter().getByGeneIdList(StringUtils.toList(query, ",")));
 			/** HQL 
 			Query query = this.getSession().createQuery("select e from Exon e JOIN FETCH e.exon2transcripts et JOIN et.transcript t JOIN  t.gene g where g.stableId in :stable_id").setParameterList("stable_id", StringUtils.toList(geneId, ","));  
 			return generateResponse(query);
@@ -90,10 +68,9 @@ public class GeneWSServer extends GenericRestWSServer {
 	
 	@GET
 	@Path("/{geneId}/exon2transcript")
-	public Response getExon2TranscriptByEnsemblId(@PathParam("geneId") String geneId) {
+	public Response getExon2TranscriptByEnsemblId(@PathParam("geneId") String query) {
 		try {
-			Query query = this.getSession().createQuery("select e from Exon2transcript e JOIN FETCH e.transcript t JOIN FETCH e.exon JOIN FETCH t.gene g where g.stableId in :stable_id").setParameterList("stable_id", StringUtils.toList(geneId, ","));
-			return generateResponse(query);
+			return generateResponse(query, new Exon2TranscriptDBAdapter().getByGeneIdList(StringUtils.toList(query, ",")));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
@@ -102,37 +79,13 @@ public class GeneWSServer extends GenericRestWSServer {
 	
 	@GET
 	@Path("/{geneId}/orthologous")
-	public Response getOrthologousByEnsemblId(@PathParam("geneId") String geneId) {
+	public Response getOrthologousByEnsemblId(@PathParam("geneId") String query) {
 		try {
-			Criteria criteria =  this.getSession().createCriteria(Orthologous.class)
-			.createCriteria("gene").add( Restrictions.eq("stableId", geneId)).setFetchMode("gene", FetchMode.DEFAULT);
-			return generateResponse(criteria);
+			return generateResponse(query, new OrthologousDBAdapter().getByGeneIdList(StringUtils.toList(query, ",")));
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.error("", StringUtils.getStackTrace(e));
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-
-
-//	@GET
-//	@Path("/{geneId}/sequence")
-//	public Response getSequenceByEnsemblId(@PathParam("geneId") String geneId) {
-//		try {
-//			Criteria criteria =  this.getSession().createCriteria(Gene.class).add(Restrictions.eq("stableId", geneId));
-//			String sequence = new String();
-//			if (criteria.list().size() > 0){
-//				Gene gene = (Gene)criteria.list().get(0);
-//				sequence = GenomeSequenceDataAdapter.getSequenceByRegion(gene.getChromosome(), gene.getStart(), gene.getEnd());
-//			}
-//			this.getSession().close();
-//			
-//			return Response.ok(sequence).build();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-//		}
-//	}
-
-
 }
--- src/main/java/org/bioinfo/infrared/ws/server/rest/genomic/RegionWSServer.java
+++ src/main/java/org/bioinfo/infrared/ws/server/rest/genomic/RegionWSServer.java
@@ -10,16 +10,18 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-
+import org.bioinfo.infrared.common.dao.Region;
 import org.bioinfo.infrared.core.Cytoband;
 import org.bioinfo.infrared.core.Exon;
 import org.bioinfo.infrared.core.Gene;
-import org.bioinfo.infrared.core.GeneDataAdapter;
+import org.bioinfo.infrared.core.GeneDBAdapter;
+import org.bioinfo.infrared.core.ExonDBAdapter;
+import org.bioinfo.infrared.core.CytobandDBAdapter;
 import org.bioinfo.infrared.core.Orthologous;
 import org.bioinfo.infrared.core.Snp;
 import org.bioinfo.infrared.core.Transcript;
-import org.bioinfo.infrared.dao.GenomeSequenceDataAdapter;
-import org.bioinfo.infrared.dao.Region;
+import org.bioinfo.infrared.core.TranscriptDBAdapter;
+import org.bioinfo.infrared.common.dao.GenomeSequenceDataAdapter;
 import org.bioinfo.infrared.ws.server.rest.GenericRestWSServer;
 import org.bioinfo.infrared.ws.server.rest.exception.VersionException;
 import org.hibernate.Criteria;
@@ -31,7 +33,7 @@ import org.hibernate.criterion.Restrictions;
 import com.sun.jersey.api.client.ClientResponse.Status;
 
 
-@Path("/{version}/{species}/chregion/")
+@Path("/{version}/{species}/genomic/chregion/")
 @Produces("text/plain")
 public class RegionWSServer extends GenericRestWSServer {
 	public RegionWSServer(@PathParam("version") String version, @PathParam("species") String species, @Context UriInfo uriInfo) throws VersionException, IOException {
@@ -42,7 +44,7 @@ public class RegionWSServer extends GenericRestWSServer {
 	@Path("/{chrRegionId}/gene")
 	public Response getGenesByRegion(@PathParam("chrRegionId") String chregionId) {
 		try {
-			return generateResponse(chregionId, new GeneDataAdapter().getGeneByRegion(chregionId));
+			return generateResponse(chregionId, new GeneDBAdapter().getGeneByRegion(chregionId));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
@@ -53,32 +55,19 @@ public class RegionWSServer extends GenericRestWSServer {
 	@Path("/{chrRegionId}/transcript")
 	public Response getTranscriptByRegion(@PathParam("chrRegionId") String chregionId) {
 		try {
-			Criteria criteria =  this.getSession().createCriteria(Transcript.class);
-			return getFeaturesByRegion(chregionId, criteria);
+			return generateResponse(chregionId, new TranscriptDBAdapter().getByRegionList(chregionId));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
 	
-	@GET
-	@Path("/{chrRegionId}/snp")
-	public Response getSnpByRegion(@PathParam("chrRegionId") String chregionId) {
-		try {
-			Criteria criteria =  this.getSession().createCriteria(Snp.class);
-			return getFeaturesByRegion(chregionId, criteria);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		}
-	}
 	
 	@GET
 	@Path("/{chrRegionId}/exon")
 	public Response getExonByRegion(@PathParam("chrRegionId") String chregionId) {
 		try {
-			Criteria criteria =  this.getSession().createCriteria(Exon.class);
-			return getFeaturesByRegion(chregionId, criteria);
+			return generateResponse(chregionId, new ExonDBAdapter().getExonByRegionList(chregionId));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
@@ -89,20 +78,33 @@ public class RegionWSServer extends GenericRestWSServer {
 	@Path("/{chrRegionId}/cytoband")
 	public Response getCytobandByRegion(@PathParam("chrRegionId") String chregionId) {
 		try {
-			Criteria criteria =  this.getSession().createCriteria(Cytoband.class);
-			return getFeaturesByRegion(chregionId, criteria);
+			List<Region> regions = Region.parseRegions(chregionId);
+			return generateResponse(chregionId, new CytobandDBAdapter().getByRegionList(regions));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
 	
+//	
+//	@GET
+//	@Path("/{chrRegionId}/cytoband")
+//	public Response getCytobandByRegion(@PathParam("chrRegionId") String chregionId) {
+//		try {
+//			Criteria criteria =  this.getSession().createCriteria(Cytoband.class);
+//			return getFeaturesByRegion(chregionId, criteria);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+//		}
+//	}
+	
 	@GET
 	@Path("/{chrRegionId}/sequence")
 	public Response getSequenceByRegion(@PathParam("chrRegionId") String chregionId) {
 		try {
-			Region region = Region.parseRegion(chregionId);
-			List sequences = GenomeSequenceDataAdapter.getSequenceByRegion(region.getChromosome(), region.getStart(), region.getEnd());
+			List<Region> regions = Region.parseRegions(chregionId);
+			List sequences =  GenomeSequenceDataAdapter.getByRegionList(regions);
 			return this.generateResponse(chregionId, sequences);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -110,21 +112,25 @@ public class RegionWSServer extends GenericRestWSServer {
 		}
 	}
 	
-	private Response getFeaturesByRegion(String chregionId, Criteria criteria){
-		try {
-			List<Region> regions = Region.parseRegions(chregionId);
-			Disjunction disjunction = Restrictions.disjunction();
-			for (Region region : regions) {
-				Conjunction disjunctionRegion = Restrictions.conjunction();
-				disjunctionRegion.add(Restrictions.eq("chromosome", region.getChromosome())).add( Restrictions.ge("start", region.getStart())).add(Restrictions.le("end", region.getEnd()));
-				disjunction.add(disjunctionRegion);
-			}
-			criteria.add(disjunction);
-			return generateResponse(criteria);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		}
-	}
+	
+	
+//	
+//	
+//	private Response getFeaturesByRegion(String chregionId, Criteria criteria){
+//		try {
+//			List<Region> regions = Region.parseRegions(chregionId);
+//			Disjunction disjunction = Restrictions.disjunction();
+//			for (Region region : regions) {
+//				Conjunction disjunctionRegion = Restrictions.conjunction();
+//				disjunctionRegion.add(Restrictions.eq("chromosome", region.getChromosome())).add( Restrictions.ge("start", region.getStart())).add(Restrictions.le("end", region.getEnd()));
+//				disjunction.add(disjunctionRegion);
+//			}
+//			criteria.add(disjunction);
+//			return generateResponse(criteria);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+//		}
+//	}
 
 }
