63fbcacd7824f00f053a9d3cdf6905faea97a074
Alex <ademaria@cipf.es>
2011-11-24 11:58:31 +0100

    asda

--- src/main/java/org/bioinfo/infrared/ws/server/rest/FeatureExclusionStrategy.java
+++ src/main/java/org/bioinfo/infrared/ws/server/rest/FeatureExclusionStrategy.java
@@ -7,17 +7,11 @@ public class FeatureExclusionStrategy implements ExclusionStrategy {
 
 	@Override
 	public boolean shouldSkipClass(Class<?> arg0) {
-//		System.out.println(arg0.getName());
-//		if (arg0.isInterface()){
-//			
-//			return true;
-//		}
 		return false;
 	}
 
 	@Override
 	public boolean shouldSkipField(FieldAttributes arg0) {
-//		System.out.println(arg0.getDeclaredType().toString());
 		if (arg0.getDeclaredType().toString().contains("infrared")){
 			return true;
 		}
